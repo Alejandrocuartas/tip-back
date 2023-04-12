3b443206c2276caf399982f6bf4cc0248f40fdb6
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-04-12 14:00:24 -0500

    style

--- src/use-cases/employee/assist.ts
+++ src/use-cases/employee/assist.ts
@@ -2,22 +2,23 @@ import mongoose from "mongoose";
 import Day from "../../entities/day";
 import Employee from "../../entities/employee";
 
-const assist = async(cc: string, date: string, isDay: boolean = false) => {
+const assist = async (cc: string, date: string, isDay: boolean = false) => {
     const employee = await Employee.findOne({ cc })
-    const day = await Day.findOne({ $and: [{date}, {isDay}] });
+    const day = await Day.findOne({ $and: [{ date }, { isDay }] });
     //validate employee is not null
     if (employee == null) {
         throw new Error("Employee not found");
     }
-    if(day == null) {
+    if (day == null) {
         throw new Error("Day not found");
     }
     const alreadyMarked = day.employees.includes(employee._id)
-    if(!alreadyMarked){
+    let newD;
+    if (!alreadyMarked) {
         day.employees.push(employee._id)
-        await day.save()
+        newD = (await day.save()).populate("employees")
     }
-    return day
+    return newD
 }
 
 export default assist;
