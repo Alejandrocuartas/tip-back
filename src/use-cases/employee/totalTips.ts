01caea1d4c2489cfee4e99071677fc563a393cd9
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2023-05-03 13:10:09 -0500

    spaces

--- src/use-cases/employee/totalTips.ts
+++ src/use-cases/employee/totalTips.ts
@@ -1,17 +1,17 @@
 import Day from "../../entities/day";
 import Employee from "../../entities/employee";
 
-const getTotalTips = async(cc: string) => {
+const getTotalTips = async (cc: string) => {
     const employee = await Employee.findOne({ cc })
-    if(!employee){
+    if (!employee) {
         throw new Error("cc invalid");
     }
     const days = await Day.find({ employees: employee._id })
     let totalTips: number = 0;
     days.forEach(day => {
-        const dayTips = Number(day.tips/day.employees.length)
+        const dayTips = Number(day.tips / day.employees.length)
         totalTips += dayTips
-        
+
     })
     return totalTips;
 }
